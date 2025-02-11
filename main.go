8a68b763eacaa2ab02c57079ea8a2d4ea38ee577
Nikkhil369 <159174612+Nikkhil369@users.noreply.github.com>
2025-02-11 15:34:59 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prakyy/Go-Drive/controllers"
+	"github.com/prakyy/Go-Drive/initializers"
+	"github.com/prakyy/Go-Drive/middleware"
+)
+
+func init() {
+	initializers.LoadEnv()
+	initializers.LinkDB()
+	initializers.SyncDB()
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	r.POST("/upload", middleware.RequireAuth, controllers.ReceiveFile)
+	r.GET("/files/:file_id", middleware.RequireAuth, controllers.ServeFile)
+	r.DELETE("/delete/:file_id", middleware.RequireAuth, controllers.DeleteFile)
+	r.GET("/search", middleware.RequireAuth, controllers.SearchFile)
+	r.Run() // localhost:3000
+}
